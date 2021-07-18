df8c18226ae210bba9be139dc9253144cbd270d4
JamKiller <tktetb@163.com>
2021-07-18 17:51:28 +0800

    Health

--- 2DShoot/Assets/Scripts/Health.cs
+++ 2DShoot/Assets/Scripts/Health.cs
@@ -1,18 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+/**
+ * @author BoLuo
+ * @email [ tktetb@163.com ]
+ * @create date 17:09:16
+ * @modify date 17:09:16
+ * @desc []
+ */
+
+using System;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+namespace DefaultNamespace
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 血量
+    /// todo 对值做一些访问限制
+    /// </summary>
+    public class Health : MonoBehaviour,IHealth
     {
-        
-    }
+        #region fields
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Tooltip("当前血量")]
+        public int CurrentHealth;
+
+        [Tooltip("初始血量")]
+        public int InitialHealth = 10;
+
+        [Tooltip("最大血量")]
+        public int MaximumHealth = 10;
+
+        /// <summary>
+        /// 上次受到的伤害值
+        /// </summary>
+        public int LastDamage { get; set; }
+
+        #endregion
+
+        #region public
+
+        #endregion
+
+        private void Awake()
+        {
+            Initialization();
+        }
+
+        public void Damage(int damage,
+                           GameObject instigator)
+        {
+            // 血量已经低于0
+            if ((CurrentHealth <= 0) && (InitialHealth != 0))
+            {
+                return;
+            }
+
+            // 减去伤害
+            CurrentHealth -= damage;
+
+            // 如果血量小于0
+            if (CurrentHealth <= 0)
+            {
+                // 设置为0
+                CurrentHealth = 0;
+            }
+
+            LastDamage = damage;
+        }
+
+        public void Initialization()
+        {
+            CurrentHealth = InitialHealth;
+        }
     }
 }
---
+++ 2DShoot/Assets/Scripts/IHealth.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public interface IHealth
+{
+    /// <summary>
+    /// 造成伤害
+    /// </summary>
+    /// <param name="damage">伤害量</param>
+    /// <param name="instigator">发起伤害者</param>
+    void Damage(int damage,
+                GameObject instigator);
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    void Initialization();
+}
